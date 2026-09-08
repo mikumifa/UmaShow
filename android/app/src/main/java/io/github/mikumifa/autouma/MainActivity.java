00dc4d7df621b7869183e3b98dce8a82f36f721d
mikumifa <1055069518@qq.com>
2026-09-09 04:07:10 +0800

    v1.4.31

--- android/app/src/main/java/io/github/mikumifa/autouma/MainActivity.java
+++ android/app/src/main/java/io/github/mikumifa/autouma/MainActivity.java
@@ -3,13 +3,8 @@ package io.github.mikumifa.autouma;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.WindowManager;
-import android.webkit.WebView;
 
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.getcapacitor.BridgeActivity;
@@ -19,16 +14,11 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
-        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+        int systemBarColor = Color.rgb(248, 250, 252);
+        getWindow().setStatusBarColor(systemBarColor);
+        getWindow().setNavigationBarColor(systemBarColor);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            WindowManager.LayoutParams attributes = getWindow().getAttributes();
-            attributes.layoutInDisplayCutoutMode =
-                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-            getWindow().setAttributes(attributes);
-        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             getWindow().setStatusBarContrastEnforced(false);
             getWindow().setNavigationBarContrastEnforced(false);
@@ -40,21 +30,5 @@ public class MainActivity extends BridgeActivity {
         );
         controller.setAppearanceLightStatusBars(true);
         controller.setAppearanceLightNavigationBars(true);
-
-        WebView webView = getBridge().getWebView();
-        ViewCompat.setOnApplyWindowInsetsListener(webView, (view, windowInsets) -> {
-            Insets insets = windowInsets.getInsets(
-                WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout()
-            );
-            webView.post(() -> webView.evaluateJavascript(
-                "document.documentElement.style.setProperty('--autouma-native-safe-top','" + insets.top + "px');" +
-                "document.documentElement.style.setProperty('--autouma-native-safe-right','" + insets.right + "px');" +
-                "document.documentElement.style.setProperty('--autouma-native-safe-bottom','" + insets.bottom + "px');" +
-                "document.documentElement.style.setProperty('--autouma-native-safe-left','" + insets.left + "px');",
-                null
-            ));
-            return windowInsets;
-        });
-        ViewCompat.requestApplyInsets(webView);
     }
 }
